7970a9740b674df231a7d77496aebec7c0a6b315
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-03-28 20:43:29 -0300

    R01 - Bubble, Insertion and Selection Sort.

--- R01-01-Rot-SimpleSorting-Simultaneous-Bubble-environment/src/test/java/sorting/test/StudentSortingTest.java
+++ R01-01-Rot-SimpleSorting-Simultaneous-Bubble-environment/src/test/java/sorting/test/StudentSortingTest.java
@@ -43,7 +43,7 @@ public class StudentSortingTest {
 	private void getImplementation() {
 		// TODO O aluno deve instanciar sua implementação abaixo ao invés de
 		// null
-		this.implementation = new SelectionSort<Integer>();
+		this.implementation = new SimultaneousBubblesort<Integer>();
 		//Assert.fail("Implementation not provided");
 	}
 
