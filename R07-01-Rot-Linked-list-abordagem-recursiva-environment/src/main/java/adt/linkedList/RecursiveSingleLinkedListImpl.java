50ba891e00fb4e64412c3f82d15b525b22c06594
raianyrufino <raiany.paz@ccc.ufcg.edu.br>
2019-04-25 20:01:27 -0300

    R07

---
+++ R07-01-Rot-Linked-list-abordagem-recursiva-environment/src/main/java/adt/linkedList/RecursiveSingleLinkedListImpl.java
@@ -0,0 +1,94 @@
+package adt.linkedList;
+
+public class RecursiveSingleLinkedListImpl<T> implements LinkedList<T> {
+
+	protected T data;
+	protected RecursiveSingleLinkedListImpl<T> next;
+
+	public RecursiveSingleLinkedListImpl() {
+
+	}
+
+	public RecursiveSingleLinkedListImpl(T data, RecursiveSingleLinkedListImpl<T> next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.data == null;
+	}
+
+	@Override
+	public int size() {
+		if(this.data == null){
+			return 0;
+		}
+		return 1 + next.size();
+	}
+
+	@Override
+	public T search(T element) {
+		if(isEmpty()){
+			return null;
+		}
+
+		if(this.data.equals(element)){
+			return this.data;
+		}
+		return next.search(element);
+	}
+
+	@Override
+	public void insert(T element) {
+		if (isEmpty()) {
+			this.data = element;
+			this.next = new RecursiveSingleLinkedListImpl<>();
+		} else {
+			next.insert(element);
+		}
+	}
+
+	@Override
+	public void remove(T element) {
+		if(!isEmpty() && element != null){
+			if(this.data.equals(element)){
+				this.data = next.data;
+				this.next = next.next;
+			} else {
+				this.next.remove(element);
+			}
+		}
+	}
+
+	@Override
+	public T[] toArray() {
+		T[] saida = (T[]) new Object[this.size()];
+		buildArray(saida, 0, this);
+		return saida;
+	}
+
+	private void buildArray(T[] array, int pos, RecursiveSingleLinkedListImpl<T> node) {
+		if (!node.isEmpty()) {
+			array[pos] = node.getData();
+			buildArray(array, ++pos, node.getNext());
+		}
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public RecursiveSingleLinkedListImpl<T> getNext() {
+		return next;
+	}
+
+	public void setNext(RecursiveSingleLinkedListImpl<T> next) {
+		this.next = next;
+	}
+
+}
---
+++ R07-01-Rot-Linked-list-abordagem-recursiva-environment/src/main/java/adt/stack/StackRecursiveDoubleLinkedListImpl.java
@@ -0,0 +1,54 @@
+package adt.stack;
+
+import adt.linkedList.DoubleLinkedList;
+import adt.linkedList.RecursiveDoubleLinkedListImpl;
+
+public class StackRecursiveDoubleLinkedListImpl<T> implements Stack<T> {
+
+	protected DoubleLinkedList<T> top;
+	protected int size;
+
+	public StackRecursiveDoubleLinkedListImpl(int size) {
+		this.size = size;
+		this.top = new RecursiveDoubleLinkedListImpl<T>();
+	}
+
+	@Override
+	public void push(T element) throws StackOverflowException {
+		if (!isFull()) {
+			top.insertFirst(element);
+		} else {
+			throw new StackOverflowException();
+		}
+	}
+
+	@Override
+	public T pop() throws StackUnderflowException {
+		if (!isEmpty()) {
+			top.removeFirst();
+		}
+		throw new StackUnderflowException();
+	}
+
+	@Override
+	public T top() {
+		if (!isEmpty()) {
+			return ((RecursiveDoubleLinkedListImpl<T>) this.top).getData();
+		}
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		if (top.isEmpty()) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public boolean isFull() {
+		return top.size() == this.size;
+	}
+
+}
